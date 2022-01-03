af08722ef52c0270d1e487811c20882c1549794e
Milan <milancakic89@gmail.com>
2022-01-03 16:39:21 +0100

    feat: Stars completed

--- quiz-cms/src/app/play/play.component.ts
+++ quiz-cms/src/app/play/play.component.ts
@@ -15,46 +15,47 @@ import { PlayService } from './play.service';
 })
 export class PlayComponent implements OnDestroy, OnChanges, OnInit {
 
-  constructor(private modal: ModalWrapper, 
-              private router: Router,
-              private config: Configuration,
-              private notificationService: NotificationService,
-              private playService: PlayService,
-              private questionService: QuestionService) { }
+  constructor(private modal: ModalWrapper,
+    private router: Router,
+    private config: Configuration,
+    private notificationService: NotificationService,
+    private playService: PlayService,
+    private questionService: QuestionService) { }
 
-  get attempts(){ return this._attempts }
-  set attempts(value){this._attempts = value}
+  get attempts() { return this._attempts }
+  set attempts(value) { this._attempts = value }
 
-  get userContributions() {return this.config.user.getValue().categories as unknown as any}
+  get userContributions() { return this.config.user.getValue().categories as unknown as any }
 
   public time = 15;
   public score = 0;
 
   public questionCount = 0;
-  public question: Question | any = null;
+  public question: Question = {
+    question: '',
+    status: 'NA CEKANJU',
+    answers: [],
+    correct_letter: '',
+    type: 'REGULAR',
+    correct_text: '',
+    opened: false
+  };
   public timeInterval: any = null;
   public questionSelected = false;
   public selectedLetter = '';
   public playCategory = '';
   public showModal = true;
   public lives = 0;
-  public nextQuestionInterval = 300;
-  public currentQuestion: Question = {
-    question: '',
-    status: 'NA CEKANJU',
-    answers: [],
-    correct_letter: '',
-    correct_text: '',
-    opened: false
-  }
-  ngOnInit(){
-    this.config.user.subscribe(user =>{
-      if(user){
+  public nextQuestionInterval = 400;
+  public progressBarPercentage = 0;
+
+  ngOnInit() {
+    this.config.user.subscribe(user => {
+      if (user) {
         this.lives = user.lives;
-        console.log(user)
-        if(this.lives === 0){
-          setTimeout(()=>{
-              this.router.navigateByUrl('/profile')
+        if (this.lives === 0) {
+          setTimeout(() => {
+            this.router.navigateByUrl('/profile')
           }, 1000)
         }
       }
@@ -62,53 +63,54 @@ export class PlayComponent implements OnDestroy, OnChanges, OnInit {
   }
 
   ngOnChanges(): void {
-    this.modal.startGame.subscribe(bool =>{
-      if(bool){
+    this.modal.startGame.subscribe(bool => {
+      if (bool) {
         this.initGame();
       }
     })
 
   }
 
-  public isCategoryAllowed(category: String){
-    if(category === 'GEOGRAFIJA'){
+  public isCategoryAllowed(category: String) {
+    if (category === 'GEOGRAFIJA') {
       return true;
     }
     const userContribution = this.userContributions.find((c: any) => c.category === category);
-    if(userContribution){
+    if (userContribution) {
       return userContribution.questions_added >= 10;
     }
     return false;
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.modal.startGame.next(false);
     this.playService.allowBackButton = true;
     clearInterval(this.timeInterval);
   }
 
-  public initGame(){
+  public initGame() {
     this.modal.startGame.next(true)
     this.playService.allowBackButton = false;
     this.score = 0;
     this.time = 15;
+    this.progressBarPercentage = 0;
     this.questionCount = 0;
     this.getQuestion();
   }
 
-  public async getQuestion(){
-    if (this._questionCounter > 15 || !this.attempts.length){
+  public async getQuestion() {
+    if (this.questionCount >= 15 || !this.attempts.length) {
       this.gameOver('End of game');
     }
-    this._questionCounter++;
     const { data, success } = await this.questionService.getSingleQuestion(this.playCategory)
-    if(success){
+    if (success) {
       this.question = data;
+      this.questionCount++;
       this.selectedLetter = '';
       this.questionSelected = false;
       this.time = 15;
       this.initTime();
-    }else{
+    } else {
       clearInterval(this.timeInterval);
       this.notificationService.notification.emit({
         success: false,
@@ -118,70 +120,126 @@ export class PlayComponent implements OnDestroy, OnChanges, OnInit {
     }
   }
 
-  public initTime(){
+  public initTime() {
     clearInterval(this.timeInterval)
-    this.timeInterval = setInterval(()=>{
-      if(this.time > 0){
-       this.time--;
-      }else{
+    this.timeInterval = setInterval(() => {
+      if (this.time > 0) {
+        this.time--;
+      } else {
         this.timeWarning();
       }
-    },1000)
-}
-
-public timeWarning(){
-  this.questionSelected = true;
-  this.stopTime();
-  this.updateQuestion(false)  
-}
-
-public gameOver(message?: string){
-  this.stopTime();
-  let points = 0;
-  if(this.score > 2 && this.score <= 7){
-    points = 1;
+    }, 1000)
   }
-  if(this.score > 7 && this.score <= 11){
-    points = 2
-  }
-  if (this.score > 11 && this.score <= 14){
-    points = 3;
-  }
-  if (this.score > 14) {
-    points = 5;
-  }
-  if(this.score < 2){
-    this.reduceOneLife();
-  }
-  this.modal.gameResults.next({
-    success: points !== 0,
-    results: points,
-    showModal: true
-  });
 
-  this.playService.allowBackButton = true;
-  this.score = 0;
-  this.attempts = [1, 1, 1]
-  this.modal.startGame.next(false)
-
-}
-
-public closeModal(category: string, allowedCategory: string){
-  const allowed = this.isCategoryAllowed(allowedCategory);
-  if (!allowed){
-    return;
+  public timeWarning() {
+    this.questionSelected = true;
+    this.stopTime();
+    this.updateQuestion(false)
   }
-  this.playCategory = category;
-  
-  setTimeout(()=>{
-    this.showModal = false;
-    this.initGame();
-  }, 1600)
-  
-}
 
-  public async onSelectedAnswer(answer: Answers){
-    if(this.questionSelected){
+  public updateProgressBar() {
+    switch (this.score) {
+      case 0:
+        this.progressBarPercentage = 6;
+        break;
+      case 1:
+        this.progressBarPercentage = 6;
+        break;
+      case 2:
+        this.progressBarPercentage = 10;
+        break;
+      case 3:
+        this.progressBarPercentage = 14;
+        break;
+      case 4:
+        this.progressBarPercentage = 17;
+        break;
+      case 5:
+        this.progressBarPercentage = 21;
+        break;
+      case 6:
+        this.progressBarPercentage = 27;
+        break;
+      case 7:
+        this.progressBarPercentage = 34;
+        break;
+      case 8:
+        this.progressBarPercentage = 41;
+        break;
+      case 9:
+        this.progressBarPercentage = 48;
+        break;
+      case 10:
+        this.progressBarPercentage = 53;
+        break;
+      case 11:
+        this.progressBarPercentage = 61;
+        break;
+      case 12:
+        this.progressBarPercentage = 70;
+        break;
+      case 13:
+        this.progressBarPercentage = 80;
+        break;
+      case 14:
+        this.progressBarPercentage = 90;
+        break;
+      case 15:
+        this.progressBarPercentage = 100;
+        break;
+    }
+  }
+
+  public gameOver(message?: string) {
+    this.stopTime();
+    let points = 0;
+    if (this.score > 4 && this.score <= 7) {
+      points = 1;
+    }
+    if (this.score > 7 && this.score <= 10) {
+      points = 2
+    }
+    if (this.score > 10 && this.score <= 12) {
+      points = 3;
+    }
+    if (this.score > 12 && this.score <= 14) {
+      points = 4;
+    }
+    if (this.score > 14) {
+      points = 5;
+    }
+    if (this.score < 2) {
+      this.reduceOneLife();
+    }
+    this.modal.gameResults.next({
+      success: points !== 0,
+      results: points,
+      showModal: true
+    });
+
+    this.playService.allowBackButton = true;
+    this.score = 0;
+    this.attempts = [1, 1, 1]
+    this.modal.startGame.next(false)
+
+  }
+
+  public closeModal(category: string, allowedCategory: string) {
+    const allowed = this.isCategoryAllowed(allowedCategory);
+    if (!allowed) {
+      return;
+    }
+    this.playCategory = category;
+
+    setTimeout(() => {
+      this.showModal = false;
+      this.initGame();
+    }, 1600)
+
+  }
+
+  public async onSelectedAnswer(answer: Answers) {
+    if (this.questionSelected) {
       return;
     }
     this.questionSelected = true;
@@ -191,11 +249,12 @@ public closeModal(category: string, allowedCategory: string){
     this.updateQuestion(correct);
   }
 
-  public async updateQuestion(correct: boolean){
+  public async updateQuestion(correct: boolean) {
     const { data, success } = await this.playService.checkQuestion(this.question._id, correct)
-    if(success){
+    if (success) {
       if (data) {
         this.score++;
+        this.updateProgressBar();
       } else {
         this.reduceAttempts();
       }
@@ -207,7 +266,7 @@ public closeModal(category: string, allowedCategory: string){
         }
 
       }, this.nextQuestionInterval)
-    }else{
+    } else {
       clearInterval(this.timeInterval)
       this.playService.allowBackButton = true;
       this.notificationService.notification.emit({
@@ -220,26 +279,25 @@ public closeModal(category: string, allowedCategory: string){
     }
   }
 
-  public stopTime(){
+  public stopTime() {
     clearInterval(this.timeInterval);
   }
 
-  public async reduceOneLife(){
+  public async reduceOneLife() {
     const { data, success } = await this.playService.reduceOneLife()
-    if(success){
+    if (success) {
       this.config.user.next(data)
     }
   }
-  
-  private reduceAttempts(){
-    if(this._attempts.length > 0){
+
+  private reduceAttempts() {
+    if (this._attempts.length > 0) {
       this.attempts.pop();
     }
-    
+
   }
 
-  private _attempts = [1,1,1];
-  private _questionCounter = 1;
+  private _attempts = [1, 1, 1];
   private _userId: string = '';
 
 }
