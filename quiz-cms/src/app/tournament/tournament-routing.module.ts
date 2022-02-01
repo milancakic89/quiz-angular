21078e944372a4cf1a33fd0cbac07897af09f74e
Milan <milancakic89@gmail.com>
2022-02-01 12:00:56 +0100

    feat: Waiting next round room

--- quiz-cms/src/app/tournament/tournament-routing.module.ts
+++ quiz-cms/src/app/tournament/tournament-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from "@angular/core";
 import { Route, RouterModule } from "@angular/router";
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { PlayComponent } from "./play/play.component";
 import { RoomComponent } from "./room/room.component";
+import { WaitingOthersComponent } from "./waiting-others/waiting-others.component";
 
 const routes: Route[] = [
     {path: '', component: DashboardComponent},
+    { path: 'room/:id/waiting', component: WaitingOthersComponent },
+    { path: 'room/:id/play', component: PlayComponent },
     {path: 'room/:id', component: RoomComponent}
 ]
 @NgModule({
