a8e83f643ce0764c669aa6a5596a89dd989a0c7a
Milan <milancakic89@gmail.com>
2022-02-07 15:37:17 +0100

    style: daily bonus modal

--- quiz-cms/src/app/tournament/tournament-routing.module.ts
+++ quiz-cms/src/app/tournament/tournament-routing.module.ts
@@ -6,10 +6,10 @@ import { RoomComponent } from "./room/room.component";
 import { WaitingOthersComponent } from "./waiting-others/waiting-others.component";
 
 const routes: Route[] = [
-    {path: '', component: DashboardComponent},
+    { path: '', component: DashboardComponent},
     { path: 'room/:id/results', component: WaitingOthersComponent },
     { path: 'room/:id/play', component: PlayComponent },
-    {path: 'room/:id', component: RoomComponent}
+    { path: 'room/:id', component: RoomComponent}
 ]
 @NgModule({
     imports: [RouterModule.forChild(routes)],
