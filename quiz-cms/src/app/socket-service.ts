81f18fcf2b4ac18658b8760338e6fec4bbaa2626
Milan <milancakic89@gmail.com>
2022-03-01 15:32:18 +0100

    fix: socket events

--- quiz-cms/src/app/socket-service.ts
+++ quiz-cms/src/app/socket-service.ts
@@ -56,144 +56,113 @@ export class SocketService{
     }
     
     public setup(){
-        this._events.forEach(event =>{
-            this.socket.on(event, (data: SocketResponse) => {
-                console.log(data.event)
-                console.log(data)
-                this.socketData.next(data)
-            });
+
+        this.socket.on('CREATE_ROOM', (data: SocketResponse) =>{
+            console.log(data.event)
+            console.log(data)
+            this.socketData.next(data)
+        });
+        this.socket.on('USER_DISCONECTED', (data: SocketResponse) => {
+            console.log('USER_DISCONECTED: ', data.socket)
+            this.socketData.next(data)
+        });
+        this.socket.on('USER_CONNECTED', (data: SocketResponse) => {
+            console.log('USER_CONNECTED: ', data.user_id)
+            this.socketData.next(data)
+        });
+        this.socket.on('JOIN_ROOM', (data: SocketResponse) =>{
+            console.log(data.event)
+            console.log(data)
+            this.socketData.next(data)
+        });
+        this.socket.on('ROOM_CREATED', (data: SocketResponse) =>{
+            console.log(data.event)
+            console.log(data)
+            console.log('ROOM_CREATED')
+            this.socketData.next(data)
+        });
+        this.socket.on('JOINED_ROOM', (data: SocketResponse) =>{
+            console.log(data.event)
+            console.log(data)
+            this.socketData.next(data)
+        });
+        this.socket.on('ROOM_DONT_EXIST', (data: SocketResponse) => {
+            console.log(data.event)
+            console.log(data)
+            this.socketData.next(data)
+        });
+        this.socket.on('LEAVED_ROOM', (data: SocketResponse) =>{
+            console.log(data.event)
+            console.log(data)
+            this.socketData.next(data)
+        });
+
+        this.socket.on('TOURNAMENT_STARTING', (data: SocketResponse) =>{
+            console.log(data.event)
+            console.log(data)
+            this.socketData.next(data)
         })
-        // this.socket.on('CREATE_ROOM', (data: SocketResponse) =>{
-        //     console.log(data.event)
-        //     console.log(data)
-        //     this.socketData.next(data)
-        // });
-        // this.socket.on('USER_DISCONECTED', (data: SocketResponse) => {
-        //     console.log('USER_DISCONECTED: ', data.socket)
-        //     this.socketData.next(data)
-        // });
-        // this.socket.on('USER_CONNECTED', (data: SocketResponse) => {
-        //     console.log('USER_CONNECTED: ', data.user_id)
-        //     this.socketData.next(data)
-        // });
-        // this.socket.on('JOIN_ROOM', (data: SocketResponse) =>{
-        //     console.log(data.event)
-        //     console.log(data)
-        //     this.socketData.next(data)
-        // });
-        // this.socket.on('ROOM_CREATED', (data: SocketResponse) =>{
-        //     console.log(data.event)
-        //     console.log(data)
-        //     console.log('ROOM_CREATED')
-        //     this.socketData.next(data)
-        // });
-        // this.socket.on('JOINED_ROOM', (data: SocketResponse) =>{
-        //     console.log(data.event)
-        //     console.log(data)
-        //     this.socketData.next(data)
-        // });
-        // this.socket.on('ROOM_DONT_EXIST', (data: SocketResponse) => {
-        //     console.log(data.event)
-        //     console.log(data)
-        //     this.socketData.next(data)
-        // });
-        // this.socket.on('LEAVED_ROOM', (data: SocketResponse) =>{
-        //     console.log(data.event)
-        //     console.log(data)
-        //     this.socketData.next(data)
-        // });
+        this.socket.on('START_TOURNAMENT_QUESTION', (data: SocketResponse) =>{
+            console.log(data.event)
+            console.log(data)
+            this.socketData.next(data)
+        })
 
-        // this.socket.on('TOURNAMENT_STARTING', (data: SocketResponse) =>{
-        //     console.log(data.event)
-        //     console.log(data)
-        //     this.socketData.next(data)
-        // })
-        // this.socket.on('START_TOURNAMENT_QUESTION', (data: SocketResponse) =>{
-        //     console.log(data.event)
-        //     console.log(data)
-        //     this.socketData.next(data)
-        // })
+        this.socket.on('SELECTED_QUESTION_LETTER', (data: SocketResponse) =>{
+            console.log(data.event)
+            console.log(data)
+            this.socketData.next(data)
+        })
 
-        // this.socket.on('SELECTED_QUESTION_LETTER', (data: SocketResponse) =>{
-        //     console.log(data.event)
-        //     console.log(data)
-        //     this.socketData.next(data)
-        // })
+        this.socket.on('UPDATE_WAITING_STATUS', (data: SocketResponse) =>{
+            console.log(data.event)
+            console.log(data)
+            this.socketData.next(data)
+        });
 
-        // this.socket.on('UPDATE_WAITING_STATUS', (data: SocketResponse) =>{
-        //     console.log(data.event)
-        //     console.log(data)
-        //     this.socketData.next(data)
-        // });
+        this.socket.on('EVERYONE_ANSWERED', (data: SocketResponse) =>{
+            console.log(data.event)
+            console.log(data)
+            this.socketData.next(data)
+        });
 
-        // this.socket.on('EVERYONE_ANSWERED', (data: SocketResponse) =>{
-        //     console.log(data.event)
-        //     console.log(data)
-        //     this.socketData.next(data)
-        // });
+        this.socket.on('TOURNAMENT_FINISHED', (data: SocketResponse) =>{
+            console.log(data.event)
+            console.log(data)
+            this.socketData.next(data)
+        });
 
-        // this.socket.on('TOURNAMENT_FINISHED', (data: SocketResponse) =>{
-        //     console.log(data.event)
-        //     console.log(data)
-        //     this.socketData.next(data)
-        // });
-
-        // this.socket.on('GET_ROOM_QUESTION', (data: SocketResponse) =>{
-        //     console.log(data.event)
-        //     console.log(data)
-        //     this.socketData.next(data)
-        // });
-        // this.socket.on('GET_ROOM_RESULTS', (data: SocketResponse) => {
-        //     console.log(data.event)
-        //     console.log(data)
-        //     this.socketData.next(data)
-        // });
-        // this.socket.on('ADD_FRIEND', (data: SocketResponse) => {
-        //     console.log(data.event)
-        //     console.log(data)
-        //     this.socketData.next(data)
-        // });
-        // this.socket.on('FRIEND_ALLREADY_REQUESTED', (data: SocketResponse) => {
-        //     console.log(data.event)
-        //     console.log(data)
-        //     this.socketData.next(data)
-        // });
-        // this.socket.on('ADD_FRIEND_FAILED', (data: SocketResponse) => {
-        //     this.socketData.next(data)
-        // });
-        // this.socket.on('ACCEPT_FRIEND', (data: SocketResponse) => {
-        //     console.log(data.event)
-        //     console.log(data)
-        //     this.socketData.next(data)
-        // });
+        this.socket.on('GET_ROOM_QUESTION', (data: SocketResponse) =>{
+            console.log(data.event)
+            console.log(data)
+            this.socketData.next(data)
+        });
+        this.socket.on('GET_ROOM_RESULTS', (data: SocketResponse) => {
+            console.log(data.event)
+            console.log(data)
+            this.socketData.next(data)
+        });
+        this.socket.on('ADD_FRIEND', (data: SocketResponse) => {
+            console.log(data.event)
+            console.log(data)
+            this.socketData.next(data)
+        });
+        this.socket.on('FRIEND_ALLREADY_REQUESTED', (data: SocketResponse) => {
+            console.log(data.event)
+            console.log(data)
+            this.socketData.next(data)
+        });
+        this.socket.on('ADD_FRIEND_FAILED', (data: SocketResponse) => {
+            this.socketData.next(data)
+        });
+        this.socket.on('ACCEPT_FRIEND', (data: SocketResponse) => {
+            console.log(data.event)
+            console.log(data)
+            this.socketData.next(data)
+        });
     }
 
     public emit(eventName: EventType, data: any){
         this.socket.emit(eventName, data)
     }
-
-    private _events: EventType[] = [
-        'CREATE_ROOM' ,
-        'JOIN_ROOM' ,
-        'ROOM_CREATED' ,
-        'LOAD-ROOM-USERS' ,
-        'START_TOURNAMENT' ,
-        'SELECTED_QUESTION_LETTER' ,
-        'START_TOURNAMENT_QUESTION' ,
-        'UPDATE_WAITING_STATUS' ,
-        'TOURNAMENT_FINISHED' ,
-        'GET_ROOM_QUESTION' ,
-        'EVERYONE_ANSWERED' ,
-        'GET_ROOM_RESULTS' ,
-        'CLEAN_THE_EMPTY_ROOMS' ,
-        'ADD_FRIEND' ,
-        'FRIEND_ALLREADY_REQUESTED' ,
-        'USER_DISCONECTED' ,
-        'USER_CONNECTED' ,
-        'ADD_FRIEND_FAILED' ,
-        'DISCONNECT_USER' ,
-        'ACCEPT_FRIEND' ,
-        'SAVE_SOCKET' ,
-        'LEAVE_ROOM'
-    ]
 }
