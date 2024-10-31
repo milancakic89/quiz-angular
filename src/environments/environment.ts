8b9b7586879fcbea978133fe2618bdae3d227b3b
Milan Cakic <58387889+milancakic89@users.noreply.github.com>
2024-10-31 10:54:26 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,24 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
-import { config } from '../environment-hidden-dev';
 export const environment = {
   production: true,
-  api: config.api,
-  client_id: config.client_id,
-  client_secret: config.client_secret,
-  refresh_token: config.refresh_token,
-  firebase: config.firebaseConfig,
-  facebookAppId: config.facebookAppId,
-  socketUrl: config.socketUrl,
-  baseUrl: 'localhost:4200'
+  api: 'https://quiz-ts.onrender.com',
+  client_id: '',
+  client_secret: '',
+  refresh_token: '',
+  firebase: {
+    apiKey: "AIzaSyAiRU5RRD-MVgutdS_jBWu1JpRz-HjPqQI",
+    authDomain: "https://quiz-angular-4vs1.onrender.com",
+    projectId: "kviz-live",
+    storageBucket: "https://quiz-angular-4vs1.onrender.com",
+    messagingSenderId: "586730202183",
+    appId: "1:586730202183:web:854559903df45d30e43d62"
+  },
+  facebookAppId: '123456789',
+  socketUrl: 'https://quiz-ts.onrender.com',
+  baseUrl: 'https://quiz-ts.onrender.com'
 };
+
+
 
 
 /*
