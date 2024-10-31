6a1d8c9d80577d31d9de8e8ad0e1adee31ca3a3f
Milan Cakic <58387889+milancakic89@users.noreply.github.com>
2024-10-31 10:55:04 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
-  api: 'https://quiz-angular-4vs1.onrender.com',
+  api: 'https://quiz-ts.onrender.com',
   client_id: '',
   client_secret: '',
   refresh_token: '',
   facebookAppId: '123456789',
   firebaseConfig: {
     apiKey: "AIzaSyAiRU5RRD-MVgutdS_jBWu1JpRz-HjPqQI",
-    authDomain: "https://quiz-angular-4vs1.onrender.com",
+    authDomain: "https://quiz-ts.onrender.com",
     projectId: "kviz-live",
-    storageBucket: "https://quiz-angular-4vs1.onrender.com",
+    storageBucket: "https://quiz-ts.onrender.com",
     messagingSenderId: "586730202183",
     appId: "1:586730202183:web:854559903df45d30e43d62"
   }
